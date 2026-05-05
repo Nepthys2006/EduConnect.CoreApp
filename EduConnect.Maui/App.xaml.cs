c63058f98742ea17e1c779825c383a6c53a79fcc
Daryl James E. Padogdog <daryljames.padogdog@example.com>
2026-05-06 03:56:08 +0800

    Scaffold EduConnect MVVM Architecture and Boilerplate

---
+++ EduConnect.Maui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace EduConnect.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "EduConnect.Maui" };
+	}
+}
